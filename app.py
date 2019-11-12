240049035bcae416699249e763e11f83a96bfae2
Keshav <kumarkeshaviitb@gmail.com>
2019-11-12 15:47:43 +0530

    added app.py and index.html

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask
+app = Flask(__name__, instance_relative_config=False, template_folder="templates", static_folder="static")
+
+@app.route("/")
+def hello():
+    #return "Hello World!"
+    return render_template("index.html")
