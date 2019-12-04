0d843cc7cdf7b570c65d77329ae1377bdd998b58
Keshav <kumarkeshaviitb@gmail.com>
2019-12-04 13:46:33 +0530

    added new routes

--- app.py
+++ app.py
@@ -6,4 +6,9 @@ def hello():
     return "Hello World v2!"
     #return render_template("index.html")
 
+@app.route("/menu")
+def menu_display():
+    #return "Hello World v2!"
+    return render_template("menu.html")
+
 app.run(host = '0.0.0.0', port = '5000', debug = 'True')
